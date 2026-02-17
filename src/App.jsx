2b20b6900271df3786bc9b5f77aca883559c270b
charlizemendoza <charlizecmendoza@gmail.com>
2026-02-17 17:26:29 +0800

    Updated app vite image

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import POS from './pages/POS'
 import Login from './pages/Login'
